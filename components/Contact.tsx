2d7d61ef523084dddee3830a699f55aed179a221
sanjeet kumar <96953921+Sanjeet76@users.noreply.github.com>
2024-02-15 21:34:33 +0530

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -25,10 +25,10 @@ const Contact = () => {
     return (
         <div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center z-0'>
             <h3 className='absolut top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Contact</h3>
-              className="w-full  flex space-x-5 p-10 snap-x snap-mandatory 
+            <div
+        className="w-full  flex space-x-5 p-10 snap-x snap-mandatory 
       scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 overflow-x-scroll"
       >
-
             <div className=' flex flex-col space-y-5 '>
                 <h4 className='text-4xl font-semibold text-center'>
                     I have got just what you need.{" "}
